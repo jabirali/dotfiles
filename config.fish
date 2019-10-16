b15aea1327dc0ea0957491122bb7251862d4242d
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2019-10-16 07:20:13 +0200

    Updated the new fish config
    * Added the `fisher` package manager
    * Added a corresponding `fishfile`
    * Minor customization of fish prompt

--- config.fish
+++ config.fish
@@ -3,13 +3,21 @@
 # Path: ~/.config/fish/config.fish
 #------------------------------------------------------------
 
-# Set theme
-#theme_gruvbox dark medium
+# Package manager
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end 
 
-# Set prompt
-set -g theme_display_virtualenv no
+# Set theme
+theme_gruvbox dark medium
 set -g theme_color_scheme gruvbox
-set -g theme_display_git no
+set -g theme_powerline_fonts no
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt "▶ "
+set -g theme_display_virtualenv no
+set -g theme_display_vi no
 set -g theme_date_format "+%H:%M"
 
 # Environment variables
@@ -17,6 +25,9 @@ set EDITOR nvim
 set PATH /opt/zotero/ /opt/nomad/bin/ /opt/conda/bin $PATH
 set LC_ALL en_US.UTF-8
 set LC_NUMERIC en_US.UTF-8
+
+# Keybindings
+fish_vi_key_bindings
 
 # Local aliases
 alias onedrive="rclone --vfs-cache-mode writes mount sintef: /data/onedrive"
