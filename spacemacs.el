3b666ce9be1b6a507e4b13dab900e07f986bd51e
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2019-10-25 00:14:17 +0200

    Major refactoring and updates of the Spacemacs config
    - Groups of customization are now split out into separate functions in
      the `baba/` namespace, which are defined at the bottom of the file;
    - Eshell now destroys buffers when "pop-up processes finish", which
      is quite convenient when quickly running temporary visual commands;
    - Flycheck is now automatically enabled where applicable;
    - Spaceline now has a more minimalist VCS mode, and has been revised;
    - Vterm now lets me send Evil hjkl/JK commands translated to arrows;
    - Testing out simple keyboard shortcuts for jumping with Eyebrowse;
    - Added wolfram-alpha and counsel-search shortcuts.

--- spacemacs.el
+++ spacemacs.el
@@ -16,7 +16,10 @@
    dotspacemacs-configuration-layer-path '()
    dotspacemacs-configuration-layers
    '(
-     (auto-completion)
+     (auto-completion
+       :variables
+         auto-completion-idle-delay nil
+         auto-completion-enable-sort-by-usage t)
      (emacs-lisp)
      (git)
      (html)
@@ -63,9 +66,12 @@
      (ranger)
      (shell
        :variables
-         shell-default-shell 'eshell
+         shell-default-shell 'vterm
          shell-default-height 40
-         shell-enable-smart-eshell t)
+         shell-enable-smart-eshell t
+         eshell-banner-message ""
+         eshell-history-size 1024
+         eshell-destroy-buffer-when-process-dies t)
      (shell-scripts)
      (spell-checking
        :variables
@@ -74,6 +80,7 @@
      (syntax-checking
        :variables
          syntax-checking-enable-tooltips nil
+         flycheck-check-syntax-automatically '(mode-enabled save)
          flycheck-indication-mode nil)
      (version-control
        :variables
@@ -83,7 +90,10 @@
     )
    dotspacemacs-additional-packages
    '(
+     esh-autosuggest
+     fish-completion
      gruvbox-theme
+     wolfram
     )
    dotspacemacs-excluded-packages
    '(
@@ -172,91 +182,52 @@
   (setq custom-file "~/.emacs.d/custom.el")
 
   ;; Aesthetic revision of all themes.
-  (add-hook
-   'spacemacs-post-theme-change-hook
-   (lambda ()
-     ;; Use Ubuntu/Yaru colors for window separators
-     (set-face-foreground 'window-divider "#1d1d1d")
-     (set-face-foreground 'window-divider-first-pixel "#1d1d1d")
-     (set-face-foreground 'window-divider-last-pixel "#1d1d1d")
-     (set-face-background 'vertical-border "#1d1d1d")
-     (set-face-foreground 'vertical-border "#1d1d1d")
-     ;; Turn on window separators.
-     (setq window-divider-default-places t)
-     (setq window-divider-default-right-width 8)
-     (setq window-divider-default-bottom-width 8)
-     (window-divider-mode 1)
-     ;; Increase line spacing.
-     (setq-default line-spacing 2)
-     (setq-default line-spacing 2)
-     ;; Customize fringes and margins.
-     (setq-default fringes-outside-margins t
-                   left-fringe-width 3
-                   right-fringe-width 3
-                   left-margin-width 1
-                   right-margin-width 0)
-     ;; Disable fringe arrows on long lines.
-     (setf (cdr (assq 'truncation   fringe-indicator-alist)) '(nil nil))
-     (setf (cdr (assq 'continuation fringe-indicator-alist)) '(nil nil)))))
+  (add-hook 'spacemacs-post-theme-change-hook 'baba/customize-theme))
 
 
 (defun dotspacemacs/user-config ()
   "Custom code run after `dotspacemacs/layers'."
 
-  ;; General settings:
-  ;; Disable line-highlighting and line-wrapping. However, keep the current
-  ;; line vertically centered, to provide more context and smooth scrolling.
+  ;; Disable line-highlighting. It's distracting in buffers that can have
+  ;; varying line height (e.g. org-mode with inline equations or images),
+  ;; and it doesn't provide much that cursor highlighting doesn't already.
   (spacemacs/toggle-highlight-current-line-globally-off)
+
+  ;; Turn centered-cursor-mode on everywhere. This keeps the current line
+  ;; centered. This results in smoother vertical scrolling, removes the
+  ;; need to move view and cursor separately, and automatically provides
+  ;; the maximum possible context for the code we're currently editing.
   (spacemacs/toggle-centered-point-globally-on)
+
+  ;; Use line truncation. For lines slightly wider than the current window,
+  ;; I find it more annoying to look at wrapped lines than to scroll right.
   (spacemacs/toggle-truncate-lines-on)
   (set-default 'truncate-lines t)
 
-  ;; Spaceline settings:
-  ;; Define a minimalist modeline, where unnecessary information is removed,
-  ;; and sections don't move around more than necessary as I switch windows.
-  (setq spaceline-highlight-face-func 'spaceline-highlight-face-default)
-  (setq spaceline-workspace-numbers-unicode nil)
-  (setq spaceline-window-numbers-unicode nil)
-  (spaceline-compile
-  '(((window-number) :face highlight-face :priority 100)
-    ((buffer-id) :priority 90)
-    ((process) :when active)
-    ((line-column) :when active :priority 50))
-  '(((which-function))
-    ((anzu) :priority 95)
-    ((selection-info) :priority 95)
-    ((global) :when active)
-    ((auto-compile) :priority 85)
-    ((flycheck-error flycheck-warning flycheck-info) :when active :priority 85)
-    ((version-control) :priority 80)))
-
-  ;; Mode hooks:
-  ;; Turn on useful additional functionality in various major modes.
-  ;; Note that the pdf-view hook is necessary to avoid ugly borders.
-  (add-hook 'pdf-tools-enabled-hook
-            'pdf-view-midnight-minor-mode)
-  (add-hook 'pdf-view-mode-hook
-            (lambda ()
-              (set (make-local-variable 'evil-evilified-state-cursor) (list nil))
-              (setq left-fringe-width 1
-                    right-fringe-width 1
-                    left-margin-width 0
-                    right-margin-width 0)))
-
-  ;; Keybindings:
   ;; It is more useful to navigate horizontally than vertically with H/L,
   ;; at least when using the centered-point and truncate-lines settings.
   (define-key evil-motion-state-map (kbd "H") 'evil-scroll-left)
   (define-key evil-motion-state-map (kbd "L") 'evil-scroll-right)
 
-  ;; Miscellaneous settings:
+  ;; Load more advanced customization defined below.
+  (baba/customize-modeline)
+  (baba/customize-readers)
+  (baba/customize-shells)
+  (baba/customize-layouts)
+  (baba/leader-keys)
+
+  ;; Turn on appropriate linters for all programming modes.
+  (add-hook 'prog-mode-hook (lambda () (flycheck-mode 1)))
+
+  ;(define-key evil-motion-state-map (kbd "C-f") 'evil-avy-goto-word-1)
+
   ;; Minor tweaks that simply don't fit in anywhere else.
   (setq dired-listing-switches "-lGh1v --time-style=long-iso --group-directories-first")
+  (setq wolfram-alpha-app-id (getenv "WOLFRAM_ID"))
 
-  ;; Startup activities:
   ;; This sets the default buffers to open in every Emacs session.
   (find-file-other-window "~/Notes/TODO.org")
-  (other-window)
+  (other-window 1)
   (evil-goto-first-line))
 
 
@@ -271,6 +242,170 @@
   (spacemacs/load-spacemacs-env))
 
 
-;;------------------------------------------------------------
-;; Autogenerated content below here
-;;------------------------------------------------------------
+(defun baba/customize-theme ()
+  "Customize the graphical interface.
+Some of these modifications make Emacs look more like a minimalist tiling
+window manager, others make it fit better with the Ubuntu default theme."
+
+  ;; Use Ubuntu/Yaru colors for window separators.
+  (set-face-foreground 'window-divider "#1d1d1d")
+  (set-face-foreground 'window-divider-first-pixel "#1d1d1d")
+  (set-face-foreground 'window-divider-last-pixel "#1d1d1d")
+  (set-face-background 'vertical-border "#1d1d1d")
+  (set-face-foreground 'vertical-border "#1d1d1d")
+
+  ;; Turn on window separators.
+  (setq window-divider-default-places t)
+  (setq window-divider-default-right-width 8)
+  (setq window-divider-default-bottom-width 8)
+  (window-divider-mode 1)
+
+  ;; Increase line spacing.
+  (setq-default line-spacing 2)
+  (setq-default line-spacing 2)
+
+  ;; Customize fringes and margins.
+  (setq-default fringes-outside-margins t
+                left-fringe-width 3
+                right-fringe-width 3
+                left-margin-width 1
+                right-margin-width 0)
+
+  ;; Disable fringe arrows on long lines.
+  (setf (cdr (assq 'truncation   fringe-indicator-alist)) '(nil nil))
+  (setf (cdr (assq 'continuation fringe-indicator-alist)) '(nil nil)))
+
+
+(defun baba/customize-modeline ()
+  "Define a minimalist modeline via Spaceline.
+This function removes unnecessary information from the default setup,
+and tries to minimize the section movement during window switching."
+
+  ;; Define a segment for showing the version-control status which
+  ;; is simpler (only one character) than the default one. This is
+  ;; done by eliminating information that is most of the time not
+  ;; needed (what version control system and branch we are using),
+  ;; and reducing the representation of the rest to one character.
+  (spaceline-define-segment version-control-simple
+    (when vc-mode
+      (powerline-raw
+       (when (buffer-file-name)
+         (pcase (vc-state (buffer-file-name))
+           (`up-to-date " ")
+           (`ignored " ")
+           (`edited "~")
+           (`added "+")
+           (`removed "-")
+           (`needs-merge "!")
+           (`needs-update "!")
+           (_ "?"))))))
+
+  ;; Disable coloring of the modeline based on the current Evil state.
+  ;; The cursor coloring is sufficient to indicate state, and having the
+  ;; modeline flash in colors every time I switch state is distracting.
+  (setq spaceline-highlight-face-func 'spaceline-highlight-face-default)
+
+  ;; Disable Unicode numbers for workspace and window numbers. The Unicode
+  ;; numbers look decent, but they're too small, causing legibility issues.
+  (setq spaceline-workspace-numbers-unicode nil)
+  (setq spaceline-window-numbers-unicode nil)
+
+  ;; Compile the Spaceline. This setup contains essentially only the window
+  ;; and workspace numbers, buffer name and position, version control status,
+  ;; and flycheck status. Search and selection generates extra sections, but
+  ;; only in places that don't cause the existing sections to bounce around.
+  (spaceline-compile
+    '(((window-number) :face highlight-face :priority 100)
+      ((buffer-id version-control-simple) :priority 90)
+      ((process) :when active))
+    '(((which-function) :when active)
+      ((anzu) :when active :priority 95)
+      ((selection-info) :when active :priority 95)
+      ((global) :when active)
+      ((auto-compile) :when active :priority 85)
+      ((flycheck-error flycheck-warning flycheck-info) :when active :priority 85)
+      ((line-column) :when active :priority 50)
+      ((workspace-number) :when active :face highlight-face :priority 100))))
+
+
+(defun baba/customize-readers ()
+  "Customize the modes used for reading documents."
+
+  ;; Recolor PDF buffers to match surrounding color theme by default.
+  (add-hook 'pdf-tools-enabled-hook 'pdf-view-midnight-minor-mode)
+
+  ;; Prevent ugly borders around the recolored PDF buffers;
+  ;; this fixes a bug in the combination pdf-view + evil.
+  (add-hook 'pdf-view-mode-hook
+            (lambda ()
+              (set (make-local-variable 'evil-evilified-state-cursor) (list nil))
+              (setq left-fringe-width 1 right-fringe-width 1
+                    left-margin-width 0 right-margin-width 0))))
+
+
+(defun baba/customize-shells ()
+  "Customize the modes used to interact with terminals and shells."
+
+  ;; When spawning a `vterm', we can define a proper normal-mode, where
+  ;; hjkl are autotranslated to arrow keys, and JK are autotranslated
+  ;; to page-up/page-down. This makes any terminal application that
+  ;; understands arrow keys (fish, htop, etc.) useful from an Evil
+  ;; normal-mode, as one can drop to normal mode for navigation.
+  (evil-define-key 'normal vterm-mode-map (kbd "h") 'vterm-send-left)
+  (evil-define-key 'normal vterm-mode-map (kbd "j") 'vterm-send-down)
+  (evil-define-key 'normal vterm-mode-map (kbd "k") 'vterm-send-up)
+  (evil-define-key 'normal vterm-mode-map (kbd "l") 'vterm-send-right)
+  (evil-define-key 'normal vterm-mode-map (kbd "J") 'vterm-send-next)
+  (evil-define-key 'normal vterm-mode-map (kbd "K") 'vterm-send-prior)
+
+  ;; TODO: Figure out which of these to keep.
+  ;(when (and (executable-find "fish")
+  ;           (require 'fish-completion nil t))
+  ;  (global-fish-completion-mode))
+  ;
+  ;(setq ivy-display-functions-alist nil)
+  ;(add-hook 'eshell-mode-hook 'fish-completion-mode)
+  ;(add-hook 'eshell-mode-hook 'esh-autosuggest-mode)
+  ;(add-hook 'eshell-post-command-hook 'evil-normal-state)
+
+  ;(evil-define-key 'insert 'vterm-mode-map
+  ;  (kbd "C-h") 'vterm-send-left
+  ;  (kbd "C-j") 'vterm-send-down
+  ;  (kbd "C-k") 'vterm-send-up
+  ;  (kbd "C-l") 'vterm-send-right
+
+  ;; Define aliases for use in Eshell.
+  (defalias 'v 'eshell-exec-visual))
+
+
+(defun baba/customize-layouts ()
+  "Customize the behavior of persp-mode layouts, eyebrowse workspaces, etc."
+
+  ;; What do do when opening a new workspace. An eshell is convenient
+  ;; because I can `cd' around, including to `/ssh:' paths, and then
+  ;; do an appropriate `find-file', `projectile', or `magit' command.
+  (setq eyebrowse-new-workspace 'eshell)
+
+  ;; Keybindings for switching to a workspace.
+  (define-key evil-motion-state-map (kbd "M-1") 'eyebrowse-switch-to-window-config-1)
+  (define-key evil-motion-state-map (kbd "M-2") 'eyebrowse-switch-to-window-config-2)
+  (define-key evil-motion-state-map (kbd "M-3") 'eyebrowse-switch-to-window-config-3)
+  (define-key evil-motion-state-map (kbd "M-4") 'eyebrowse-switch-to-window-config-4)
+  (define-key evil-motion-state-map (kbd "M-5") 'eyebrowse-switch-to-window-config-5)
+  (define-key evil-motion-state-map (kbd "M-6") 'eyebrowse-switch-to-window-config-6)
+  (define-key evil-motion-state-map (kbd "M-7") 'eyebrowse-switch-to-window-config-7)
+  (define-key evil-motion-state-map (kbd "M-8") 'eyebrowse-switch-to-window-config-8)
+  (define-key evil-motion-state-map (kbd "M-9") 'eyebrowse-switch-to-window-config-9)
+  (define-key evil-motion-state-map (kbd "M-0") 'eyebrowse-switch-to-window-config-0)
+
+  ;; Keybindings for making and removing workspace.
+  (define-key evil-motion-state-map (kbd "M-RET") 'eyebrowse-create-window-config)
+  (define-key evil-motion-state-map (kbd "C-w")   'eyebrowse-close-window-config))
+
+
+(defun baba/leader-keys ()
+  "Populate the private leader-key menu (SPC o)."
+
+  ;; TODO: add an entry for "oo" to my own TODO list.
+  (spacemacs/set-leader-keys "ow" 'wolfram-alpha)
+  (spacemacs/set-leader-keys "os" 'counsel-search))
