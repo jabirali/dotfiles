efe6b5f42fcbce7f31171ee1e58bf71dc27c3b5a
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2019-10-27 15:18:02 +0100

    Miscellaneous minor changes
    The biggest ones are perhaps switching to Pure as my Fish prompt,
    and adding a visual-command-like "open in new terminal" alias for
    fish+tilix. I'm also disabling fish vi-mode for now, since that
    doesn't work well within an Emacs vterm.

--- config.fish
+++ config.fish
@@ -8,26 +8,23 @@ if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
-end 
+end
 
-# Set theme
-theme_gruvbox dark medium
-set -g theme_color_scheme gruvbox
-set -g theme_powerline_fonts no
-set -g theme_newline_cursor yes
-set -g theme_newline_prompt "▶ "
-set -g theme_display_virtualenv no
-set -g theme_display_vi no
-set -g theme_date_format "+%H:%M"
+# Color theme
+#theme_gruvbox dark medium
 
 # Environment variables
 set EDITOR nvim
-set PATH /opt/zotero/ /opt/nomad/bin/ /opt/conda/bin $PATH
+set PATH /opt/zotero/ /opt/nomad/bin/ /opt/conda/bin /opt/mpw/bin $PATH
 set LC_ALL en_US.UTF-8
 set LC_NUMERIC en_US.UTF-8
 
 # Keybindings
-fish_vi_key_bindings
+#fish_vi_key_bindings
 
-# Local aliases
-alias onedrive="rclone --vfs-cache-mode writes mount sintef: /data/onedrive"
+# Global aliases
+alias o="xdg-open"
+alias e="nvim -p"
+alias v="tilix --action=app-new-session -e"
+
+alias venv="python -m venv .venv && source .venv/bin/activate.fish && pip install --upgrade pip setuptools > /dev/null"
