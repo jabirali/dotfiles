eb6c57487d270c9ef9af709129f36bd3b416ae0c
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2020-02-09 13:58:33 +0100

    Add: Smarter LatexMk setup.
    It now runs LuaLaTeX even if frontend apps try to run PDFLaTeX.

--- latexmk.pl
+++ latexmk.pl
@@ -3,6 +3,16 @@
 # Path: ~/.config/latexmk/latexmkrc
 #------------------------------------------------------------
 
+# Always use normalized ISBN13 codes.
 $biber='biber --isbn13 --isbn-normalise %O %S';
+
+# Compile to PDF via LuaLaTeX by default.
 $pdf_mode=4;
+
+# Trick LatexMk into always using LuaLaTeX,
+# regardless of what e.g. Emacs requests.
+$lualatex='lualatex -file-line-error -synctex=1 %O %S';
+$pdflatex=$lualatex;
+
+# Continuous compilation by default.
 $preview_continuous_mode=1;
