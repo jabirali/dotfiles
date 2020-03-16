26dd139234649b0441db2e3462eb14df3adf1a34
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2020-03-16 17:48:03 +0100

    Add: New wlnvim features.

--- bin/wlnvim-html.sh
+++ bin/wlnvim-html.sh
@@ -4,29 +4,29 @@
 # pop-up terminal with Neovim in it.  This version of the script edits
 # the file as a Markdown format, and converts to HTML mime after.
 
-# Create temporary file.
-tmp=$(mktemp)
+# Generate filename.
+file="/tmp/vim-$(date +%F_%H:%M:%S)"
 
 # Get primary selection from Wayland clipboard.
-wl-paste -t text/html -p > $tmp
+wl-paste -t text/html -p > $file.html
 
 # Convert to Markdown.
 format='markdown-raw_html-native_divs-native_spans-fenced_divs-bracketed_spans-header_attributes'
-pandoc -f html --atx-headers --reference-links -t $format $tmp | sponge $tmp
+pandoc -f html --atx-headers --reference-links -t $format $file.html > $file.md
 
 # Open Kitty + Neovim to edit the selection.
-kitty --title 'Markdown editor' -e nvim +'set ft=markdown' $tmp
+kitty --title 'vim: clip' -e nvim $file.md
 
 # Convert back to HTML.
-pandoc -f markdown -t html -s $tmp | sponge $tmp
+pandoc -f markdown -t html -s --highlight-style=tango $file.md > $file.html
 
 # Prepend a meta header.
 meta='<meta http-equiv="content-type" content="text/html; charset=utf-8">'
-echo $meta | cat - $tmp | sponge $tmp
+echo $meta | cat - $file.html | sponge $file.html
 
 # Flush the result back to the clipboard
-cat $tmp | wl-copy -t text/html
-cat $tmp | wl-copy -t text/html -p
+cat $file.html | wl-copy -t text/html
+cat $file.html | wl-copy -t text/html -p
 
-# Remove temporary file
-rm $tmp
+# Make the files only readable for me.
+chmod 600 $file.{md,html}
