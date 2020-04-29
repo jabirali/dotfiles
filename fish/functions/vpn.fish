5218053c66daa7b03556a06872ece9721cf8b3c2
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2020-04-29 23:02:44 +0200

    Add: "none" and "auto" VPN options.

--- fish/functions/vpn.fish
+++ fish/functions/vpn.fish
@@ -1,20 +1,37 @@
 #!/usr/bin/env fish
 
 function vpn -d 'Connect to VPN'
-	# Disconnect from current server.
-	echo Disconnecting...
-	expressvpn disconnect &>/dev/null
+	# Check what servers are available, sorted by recommendation.
+	set rec  (expressvpn list all 1>| sed -En -e '1,/^---/ d' -e 's/(.*)\s*Y$/\1/p')
+	set rest (expressvpn list all 1>| sed -En -e '1,/^---/ d' -e 's/(.*)\s+$/\1/p')
+	set mine "auto  Reconnect to last server"  "none  Disconnect from server"
 	
 	# Select an ExpressVPN server via FZF.
-	set line (expressvpn list all 1>| sed -e '1,/^---/ d' | fzf --prompt='ExpressVPN> ')
+	set server (
+		for line in $mine $rec $rest
+			echo $line
+		end | fzf --prompt='ExpressVPN> '
+	) 
 	
 	# If a choice was made, extract name.
-	if [ -n "$line" ]
-		set name (echo $line | sed -e 's/\(\S*\).*/\1/')
+	if [ -n "$server" ]
+		set name (echo $server | sed -e 's/\(\S*\).*/\1/')
 	else
 		return 1
 	end
 	
 	# Connect to the chosen server.
-	expressvpn connect $name
+	if [ "$name" = "none" ]
+		# Disconnect from current server.
+		expressvpn disconnect
+	else if [ "$name" = "auto" ]
+		# Reconnect to previous server.
+		expressvpn disconnect &>/dev/null
+		expressvpn connect
+	else
+		# Connect to a new server.
+		echo Disconnecting...
+		expressvpn disconnect &>/dev/null
+		expressvpn connect $name
+	end
 end
