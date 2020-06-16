7443e949145b7b5329389074ddc17bc1f7fcb32c
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2020-06-16 18:02:07 +0200

    Clean: Environment variables.

--- fish/config.fish
+++ fish/config.fish
@@ -9,16 +9,8 @@ if not functions -q fisher
 	fish -c fisher
 end
 
-# System settings.
-set -x NNN_TRASH 1
-set -x NNN_USE_EDITOR 1
-set -x DOTNET_CLI_TELEMETRY_OPTOUT 1
-set -x DIRENV_LOG_FORMAT ""
-
 # Theme settings.
-set -x BAT_THEME 'ansi-light'
 # set -x FZF_DEFAULT_OPTS '--color=bg+:#fdf6e3,bg:#fdf6e3,spinner:#2aa198,hl:#268bd2,fg:#657b83,header:#268bd2,info:#b58900,border:#d7d7af,pointer:#2aa198,marker:#2aa198,fg+:#073642,prompt:#b58900,hl+:#268bd2 --layout=reverse'
-set -x NNN_CONTEXT_COLORS 5555
 
 # ANSI colors.
 set fish_color_autosuggestion normal
