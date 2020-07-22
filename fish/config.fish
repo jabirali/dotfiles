e272c3a29d9cc1da553ada8c19c274928d06990c
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2020-07-22 16:32:14 +0200

    Clean: Moved SwayWM experiments to separate branch.
    This commit restores a simplified dotfiles repo for Gnome + Tmux
    instead of SwayWM. My previous setup is kept in a separate branch.

--- fish/config.fish
+++ fish/config.fish
@@ -9,11 +9,6 @@ if not functions -q fisher
 	fish -c fisher
 end
 
-# Environment variables.
-set -x FZF_DEFAULT_OPTS '--color=bg+:#292D3E,bg:#292D3E,spinner:#89DDFF,hl:#82AAFF,fg:#8796B0,header:#82AAFF,info:#FFCB6B,pointer:#89DDFF,marker:#89DDFF,fg+:#959DCB,prompt:#FFCB6B,hl+:#82AAFF --reverse --prompt="❯ " --pointer="❯" --marker="+"'
-set -x BEMENU_OPTS '--fn "Iosevka SS09 12" --tb "#292D3E" --tf "#82AAFF" --hb "#292D3E" --hf "#FFCB6B" --nb "#292D3E" --nf "#8796B0" --fb "#292D3E" --ff "#959DCB" --ignorecase -p "❯"'
-set -x XDG_CURRENT_DESKTOP kde
-
 # ANSI colors.
 set fish_color_autosuggestion normal
 set fish_color_command blue
@@ -28,6 +23,13 @@ set fish_color_quote yellow
 set fish_color_redirection green
 set fish_color_user brgreen
 
+# Integrate with `fzf`.
+fzf_key_bindings
+
+# Integrate with `direnv`.
+eval (direnv hook fish)
+__direnv_export_eval
+
 # Plugin settings.
 set projector_dir ~/Code
 set projector_cmd edit +GFiles
@@ -39,17 +41,6 @@ alias mkvenv 'echo layout_python >> .envrc; direnv allow'
 alias pytest 'fd \'.py$\' | entr pytest'
 alias wget   'wget -e robots=off'
 alias boox   'bluetooth-sendto --device=22:22:3B:D5:62:2C'
-
-# Integrate external tools.
-fzf_key_bindings
-eval (direnv hook fish)
-__direnv_export_eval
-
-function venv-exec --description 'Run command in virtualenv'
-	source "$HOME/.cache/virtualenvs/$argv[1]/bin/activate.fish"
-	eval $argv[2..-1]
-	deactivate
-end
 
 prefer 'exa -T'   'lsd --tree' 'tree'
 prefer 'exa -l'   'lsd -l'     'ls -l'  'll'
