157fafe82be8320925d251c1e5d0398c74096f4b
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2020-08-12 23:04:19 +0200

    Feat: Fixed bug in `abbrfile`.

--- fish/config.fish
+++ fish/config.fish
@@ -55,8 +55,3 @@ prefer 'fd'       'fdfind'     'find'
 prefer 'rg'       'ag'         'grep'
 prefer 'htop'     'top'
 prefer 'parallel' 'xargs'
-
-abbr -ga b   brew
-abbr -ga bi  brew bundle install --global
-abbr -ga bu  brew bundle cleanup --global --force
-abbr -ga bs  brew search
