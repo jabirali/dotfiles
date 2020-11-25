4f1446d5d36c50247638a3be9dffba3c28049c74
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2020-11-26 00:40:27 +0100

    feat: generated doom emacs literate config
    Converted the default Doom Emacs template to a literate configuration
    file using Org mode, which tangles automatically to a `config.el`.

---
+++ doom/config.el
@@ -0,0 +1,18 @@
+;;; -*- lexical-binding: t; -*-
+
+(setq user-full-name "Jabir Ali Ouassou"
+      user-mail-address "jabirali@switzerlandmail.ch")
+
+(setq org-directory "~/iCloud/Org/"
+      org-agenda-files '("~/iCloud/Org/")
+      +org-capture-todo-file "~/iCloud/Org/Inbox.org")
+
+(setq reftex-default-bibliography "~/Library/Zotero/Library.bib"
+      bibtex-completion-bibliography '("~/Library/Zotero/Library.bib")
+      org-ref-default-bibliography '("~/Library/Zotero/Library.bib"))
+
+(setq doom-theme 'doom-one
+      doom-font "Monaco-14"
+      doom-variable-pitch-font "Monaco-14")
+
+(setq display-line-numbers-type t)
