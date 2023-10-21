5dff70aee511fd4088b92f32e7c051d43b20e45d
Jabir Ali Ouassou <jabir.ali.ouassou@hvl.no>
2023-10-22 00:09:15 +0200

    clean: miscellaneous

--- zsh/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Install Z4H
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
-
-# Custom config.
-ln -sf ~/.config/zshrc ~/.zshrc
-ln -sf ~/.config/p10k.zsh ~/.p10k.zsh
