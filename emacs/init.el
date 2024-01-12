95c3c6ffad4d4a58a562b52b64549cfd0139c98f
Jabir Ali Ouassou <jabir.ali.ouassou@hvl.no>
2024-01-13 00:38:16 +0100

    clean: major cleanup

--- emacs/init.el
+++ emacs/init.el
@@ -1,23 +1,77 @@
-;; Use 'use-package' for package management
+;;; Custom keybindings:
+;; These keybindings are setup *before* any `use-package' statements.
+;; This ensures that they remain available even if I break my config.
+
+(defun ⌘ (key cmd)
+  "Bind the chord mod+key to the given interactive command.
+
+The modifier key is automatically set to super (⌘) if Emacs is in
+GUI mode, or meta (⌥) if Emacs is running in a terminal."
+  (let* ((mod (if (display-graphic-p) "s-" "M-"))
+	 (chord (concat mod key)))
+    (global-set-key (kbd chord) cmd)))
+
+;; Emacs-like keybindings.
+(⌘ ":" 'eval-expression)
+(⌘ ";" 'comment-line)
+(⌘ "/" 'dabbrev-expand)
+(⌘ "." 'xref-find-definitions)
+(⌘ "," 'xref-go-back)
+(⌘ "b" 'switch-to-buffer)
+(⌘ "B" 'ibuffer)
+(⌘ "f" 'find-file)
+(⌘ "d" 'dired)
+(⌘ "e" 'eval-defun)
+(⌘ "g" 'magit)
+(⌘ "o" 'ace-window)
+(⌘ "k" 'kill-this-buffer)
+(⌘ "K" 'kill-some-buffers)
+(⌘ "q" 'fill-paragraph)
+(⌘ "r" 'consult-recent-file)
+(⌘ "u" 'universal-argument)
+(⌘ "x" 'execute-extended-command)
+
+;; MacOS-like keybindings.
+(⌘ "p" 'execute-extended-command)
+(⌘ "s" 'save-buffer)
+(⌘ "S" 'save-some-buffers)
+(⌘ "t" 'tab-bar-new-tab)
+(⌘ "w" 'tab-bar-close-tab)
+(⌘ "{" 'tab-bar-switch-to-prev-tab)
+(⌘ "}" 'tab-bar-switch-to-next-tab)
+(⌘ "[" 'tab-bar-history-back)
+(⌘ "]" 'tab-bar-history-forward)
+
+;; Org-mode keybindings.
+(⌘ "<return>" 'org-capture)
+(⌘ "a" 'org-agenda)
+(⌘ "i" 'baba/org-inbox)
+(⌘ "j" 'baba/org-journal)
+
+(setq mac-command-modifier 'super)
+(setq mac-option-modifier 'meta)
+
+;;; Package management:
+;; Setup 'use-package' to automatically download MELPA packages.
 (use-package use-package
   :config
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   :custom
   (use-package-always-ensure t))
 
-;; Prevent config spam.
+;; Prevent packages from spamming my config directory with junk.
+
 (use-package no-littering
   :init
   (setq user-emacs-directory (expand-file-name "~/.cache/emacs/"))
   (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
   (load custom-file))
 
-;; Load bundled modes.
+;;; Built-in packages:
 (use-package emacs
   :custom
-  (mac-command-modifier 'super)
-  (mac-option-modifier 'meta)
   (inhibit-startup-message t)
+  (truncate-lines t)
   (line-spacing 0.15)
   :custom-face
   (default ((t (:family "JetBrains Mono NL" :height 140))))
@@ -26,10 +80,26 @@
   (scroll-bar-mode -1)
   (blink-cursor-mode -1)
   (pixel-scroll-precision-mode 1)
-  :bind
-  ("s-x" . execute-extended-command)
-  ("s-;" . execute-extended-command)
-  ("s-:" . eval-expression))
+  (setq ring-bell-function 'ignore))
+
+(use-package tab-bar
+  :custom
+  (tab-bar-show 1)
+  (tab-bar-close-button-show nil)
+  (tab-bar-new-tab-choice "*scratch*")
+  (tab-bar-tab-hints t)
+  (tab-bar-select-tab-modifiers '(super))
+  (tab-bar-format '(tab-bar-format-tabs tab-bar-separator tab-bar-format-align-right tab-bar-format-global))
+  :config
+  (tab-bar-mode 1)
+  (tab-bar-history-mode 1)
+  (display-time-mode 1))
+
+(use-package outline
+  :custom
+  (outline-blank-line t)
+  :hook
+  (prog-mode . outline-minor-mode))
 
 (use-package recentf
   :config
@@ -39,28 +109,7 @@
   :config
   (savehist-mode 1))
 
-(use-package tab-bar
-  :custom
-  (tab-bar-show 1)
-  (tab-bar-close-button-show nil)
-  (tab-bar-new-tab-choice "*scratch*")
-  (tab-bar-tab-hints t)
-  (tab-bar-select-tab-modifiers '(super))
-  (tab-bar-format '(tab-bar-format-tabs tab-bar-separator))
-  :config
-  (tab-bar-mode 1)
-  (tab-bar-history-mode 1)
-  :bind
-  ;; Browser-like keybindings for tabs.
-  ("s-{" . tab-bar-switch-to-prev-tab)
-  ("s-}" . tab-bar-switch-to-next-tab)
-  ("s-w" . tab-bar-close-tab)
-  ("s-t" . tab-bar-new-tab)
-  ;; Browser-like keybindings for history.
-  ("s-[" . tab-bar-history-back)
-  ("s-]" . tab-bar-history-forward))
-
-;; Theming
+;;; Aesthetics
 (use-package doom-themes
   :config
   (load-theme 'doom-oksolar-light t))
@@ -75,12 +124,11 @@
   :config
   (doom-modeline-mode))
 
-
 (use-package spacious-padding
   :config
   (spacious-padding-mode))
 
-;; Evil mode configuration
+;;; Keybindings
 (use-package evil
   :init
   :custom
@@ -96,9 +144,29 @@
   :config
   (evil-collection-init))
 
-;; Org mode
+(use-package which-key
+  :config
+  (which-key-mode))
+
+;;; Org mode
 (use-package org
+  :config
+  (defun baba/org-inbox ()
+    "Open my task management system."
+    (interactive)
+    (find-file (expand-file-name "inbox.org" org-directory)))
+  (defun baba/org-journal ()
+    "Open my daily research journal."
+    (interactive)
+    (find-file (expand-file-name "journal.org" org-directory)))
+  :hook
+  (org-mode . visual-line-mode)
   :custom
+  (org-todo-keywords
+   '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+     (sequence "WAIT(w)" "HOLD(h)" "IDEA(*)" "|" "NOTE(-)" "STOP(s)")))
+  (org-directory "~/Sync/Org")
+  (org-agenda-files (list org-directory))
   (org-ctrl-k-protect-subtree t)
   (org-auto-align-tags nil)
   (org-startup-with-inline-images t)
@@ -129,7 +197,7 @@
   :bind (:map org-mode-map
 	      ("s-v" . org-download-clipboard)))
 
-;; Vertico, Consult, Marginalia, Orderless, and Corfu setup
+;;; Modern user experience
 (use-package vertico
   :init
   (vertico-mode))
@@ -146,76 +214,10 @@
   :init
   (setq completion-styles '(orderless)))
 
-;; (use-package corfu
-;;   :init
-;;   (global-corfu-mode))
+(use-package ace-window)
 
-;;; Spacemacs Lite.
-(use-package which-key
-  :config
-  (which-key-mode))
-  
-(use-package general
-  :config
-  ;; Define a space leader key
-  (general-auto-unbind-keys)
-  (general-create-definer my-leader-def
-    :states 'normal
-    :keymaps 'override
-    :prefix "SPC"
-    ;:non-normal-prefix "ESC SPC"
-    )
-
-  ;; Example usage of the space leader key
-  (my-leader-def
-    "SPC" '(execute-extended-command :which-key "cmd")
-    "TAB" '(switch-to-buffer :which-key "buffer")
-
-    ";" '(eval-expression :which-key "lisp")
-
-    "c" '(:ignore t :which-key "change")
-    "ct" '(consult-theme :which-key "theme")
-
-    "o" '(:ignore t :which-key "org")
-    "oo" '(org-agenda :which-key "agenda")
-    "oi" '(lambda () (interactive) (find-file-other-tab "~/Sync/Org/inbox.org") :which-key "inbox")
-    "oj" '(lambda () (interactive) (find-file-other-tab "~/Sync/Org/journal.org") :which-key "journal")
-    "ok" '(org-capture :which-key "capture")
-
-    "i" '(consult-imenu :which-key "imenu")
-
-    "h" '(:keymap help-map :which-key "help")
-
-    "w" '(:keymap evil-window-map :which-key "win")
-
-    "f" '(:ignore t :which-key "file")
-    "ff" '(find-file :which-key "find")
-    "fs" '(save-buffer :which-key "save")
-    "fk" '(kill-this-buffer :which-key "kill")
-    "fr" '(consult-recent-file :which-key "recent")
-
-    "g" '(:ignore t :which-key "git")
-    "gg" '(magit :which-key "status")
-
-    "pi" 'package-install
-    "pp" 'package-upgrade-all
-    "pr" 'package-delete
-    "pc" 'package-autoremove
-
-    "u" '(universal-argument :which-key "unarg")
-
-    "e"   '(eval-defun :which-key "eval")
-    ))
-
-;; Inbox
-(use-package ace-window
-  :bind
-  ("s-o" . 'ace-window))
-
+;;; Integrations
 (use-package magit)
 
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
-	(sequence "WAIT(w)" "HOLD(h)" "IDEA(*)" "|" "NOTE(-)" "STOP(s)")))
-
-(setq org-agenda-files '("~/Sync/Org"))
+;;; Inbox
+;; This section is for newly added and not-yet-integrated elisp.
