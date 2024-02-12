a5d698c9ef75e6ca4ba5eb9331831f67604dca13
Jabir Ali Ouassou <jabir.ali.ouassou@hvl.no>
2024-02-12 12:57:06 +0100

    fix: misc

--- emacs/init.el
+++ emacs/init.el
@@ -37,22 +37,20 @@
   (truncate-lines t)
   (use-short-answers t)
   (xterm-set-window-title t)
-  :custom-face
-  (default ((t (:family "JetBrains Mono NL" :height 140))))
   :config
   (menu-bar-mode -1)
   (blink-cursor-mode -1)
   (when (display-graphic-p)
-    (tool-bar-mode -1)
-    (scroll-bar-mode -1)
-    (fringe-mode 16))
+	(tool-bar-mode -1)
+	(scroll-bar-mode -1)
+	(fringe-mode 16))
   (recentf-mode 1)
   (savehist-mode 1))
 
 (use-package server
   :config
   (unless (server-running-p)
-    (server-mode 1)))
+	(server-mode 1)))
 
 (setq-default left-margin-width 1 right-margin-width 1)
 (set-window-buffer nil (current-buffer))
@@ -60,26 +58,26 @@
 (defadvice load-theme (after run-after-load-theme-hook activate)
   "Personal customizations of any Emacs theme that is loaded."
   (let ((bg0 (face-attribute 'default :background))
-        (bg1 (face-attribute 'scroll-bar :background))
-        (fg1 (face-attribute 'success :foreground))
-        (fg2 (face-attribute 'mode-line :foreground)))
+		(bg1 (face-attribute 'scroll-bar :background))
+		(fg1 (face-attribute 'success :foreground))
+		(fg2 (face-attribute 'mode-line :foreground)))
 
-    ;; Make the colorization of the tab bar, mode line, and dividers more minimal.
-    (set-face-attribute 'tab-bar nil :foreground bg1 :background bg1 :box `(:line-width 6 :color ,bg1))
-    (set-face-attribute 'tab-bar-tab nil :foreground fg1 :background bg1 :box `(:line-width 6 :color ,bg1))
-    (set-face-attribute 'tab-bar-tab-inactive nil :foreground fg2 :background bg1 :box `(:line-width 6 :color ,bg1))
+	;; Make the colorization of the tab bar, mode line, and dividers more minimal.
+	(set-face-attribute 'tab-bar nil :foreground bg1 :background bg1 :box `(:line-width 6 :color ,bg1))
+	(set-face-attribute 'tab-bar-tab nil :foreground fg1 :background bg1 :box `(:line-width 6 :color ,bg1))
+	(set-face-attribute 'tab-bar-tab-inactive nil :foreground fg2 :background bg1 :box `(:line-width 6 :color ,bg1))
 
-    (set-face-attribute 'mode-line nil :background bg1 :box `(:line-width 6 :color ,bg1))
-    (set-face-attribute 'mode-line-inactive nil :background bg1 :box `(:line-width 6 :color ,bg1))
+	(set-face-attribute 'mode-line nil :background bg1 :box `(:line-width 6 :color ,bg1))
+	(set-face-attribute 'mode-line-inactive nil :background bg1 :box `(:line-width 6 :color ,bg1))
 
-    (set-face-attribute 'fringe nil :foreground bg0 :background bg0)
-    (set-face-attribute 'vertical-border nil :foreground bg1 :background bg1))
+	(set-face-attribute 'fringe nil :foreground bg0 :background bg0)
+	(set-face-attribute 'vertical-border nil :foreground bg1 :background bg1))
 
   ;; Make the Kitty theme the current Emacs theme.
   (shell-command
    (let* ((emacs-theme-name (symbol-name (car custom-enabled-themes)))
-          (kitty-theme-name (capitalize (replace-regexp-in-string "-" " " emacs-theme-name))))
-     (format "kitty +kitten themes %s" kitty-theme-name))))
+		  (kitty-theme-name (capitalize (replace-regexp-in-string "-" " " emacs-theme-name))))
+	 (format "kitty +kitten themes %s" kitty-theme-name))))
 
 (defun +insert-date ()
   "Insert an ISO date stamp corresponding to today."
@@ -134,14 +132,14 @@
   ;; Prepare Spacemacs-like leader keymaps. Here, "gmap" and "lmap"
   ;; refers to a global map (leader) and local map (localleader).
   (general-create-definer gmap
-    :keymaps 'override
-    :states '(motion normal visual)
-    :prefix "SPC")
+	:keymaps 'override
+	:states '(motion normal visual)
+	:prefix "SPC")
 
   (general-create-definer lmap
-    :keymaps 'override
-    :states '(motion normal visual)
-    :prefix ",")
+	:keymaps 'override
+	:states '(motion normal visual)
+	:prefix ",")
 
   ;; Work around keyboard layout differences.
   (define-key key-translation-map (kbd "§") (kbd "`"))
@@ -152,104 +150,104 @@
 
   ;; Map "SPC" to my custom "space menu" leader map.
   (gmap
-    "SPC" '(execute-extended-command :which-key "cmd")
+	"SPC" '(execute-extended-command :which-key "cmd")
 
-    "1" '(tab-bar-select-tab :which-key "1")          ; Tmux: C-b 1
-    "2" '(tab-bar-select-tab :which-key "2")          ; Tmux: C-b 2
-    "3" '(tab-bar-select-tab :which-key "3")          ; Tmux: C-b 3
-    "4" '(tab-bar-select-tab :which-key "4")          ; Tmux: C-b 4
-    "5" '(tab-bar-select-tab :which-key "5")          ; Tmux: C-b 5
-    "6" '(tab-bar-select-tab :which-key "6")          ; Tmux: C-b 6
-    "7" '(tab-bar-select-tab :which-key "7")          ; Tmux: C-b 7
-    "8" '(tab-bar-select-tab :which-key "8")          ; Tmux: C-b 8
-    "9" '(tab-bar-select-tab :which-key "9")          ; Tmux: C-b 9
+	"1" '(tab-bar-select-tab :which-key "1")          ; Tmux: C-b 1
+	"2" '(tab-bar-select-tab :which-key "2")          ; Tmux: C-b 2
+	"3" '(tab-bar-select-tab :which-key "3")          ; Tmux: C-b 3
+	"4" '(tab-bar-select-tab :which-key "4")          ; Tmux: C-b 4
+	"5" '(tab-bar-select-tab :which-key "5")          ; Tmux: C-b 5
+	"6" '(tab-bar-select-tab :which-key "6")          ; Tmux: C-b 6
+	"7" '(tab-bar-select-tab :which-key "7")          ; Tmux: C-b 7
+	"8" '(tab-bar-select-tab :which-key "8")          ; Tmux: C-b 8
+	"9" '(tab-bar-select-tab :which-key "9")          ; Tmux: C-b 9
 
-    "a" '(org-agenda :which-key "agenda")             ; Emacs: C-c a
-    "b" '(switch-to-buffer :which-key "buffer")       ; Emacs: C-x b
-    "d" '(dired-jump :which-key "dired")              ; Emacs: C-x d
-    "f" '(find-file :which-key "file")                ; Emacs: C-x C-f
-    "g" '(magit :which-key "git")                     ; Emacs: C-x g
-    "h" `(,help-map :which-key "help")                ; Emacs: C-h
-    "i" '(imenu :which-key "imenu")                   ; Emacs: M-g i
-    "j" '(bookmark-jump :which-key "jump")
-    "k" '(kill-this-buffer :which-key "kill")         ; Emacs: C-x k
-    "n" `(,narrow-map :which-key "narrow")            ; Emacs: C-x n
-    "o" '(ace-window :which-key "other")              ; Emacs: C-x o
-    "p" `(,project-prefix-map :which-key "project")   ; Emacs: C-x p
-    "q" '(evil-window-delete :which-key "quit")       ; Vim: :q
-    "r" '(recentf :which-key "recent")                ; Emacs: C-c r
-    "s" '(save-buffer :which-key "save")              ; Emacs: C-x s
-    "t" '(tab-bar-new-tab :which-key "tab")           ; Emacs: C-x t n
-    "w" `(,evil-window-map :which-key "window")       ; Vim: C-w
-    "y" '(clone-indirect-buffer-other-window :which-key "indirect"))
+	"a" '(org-agenda :which-key "agenda")             ; Emacs: C-c a
+	"b" '(switch-to-buffer :which-key "buffer")       ; Emacs: C-x b
+	"d" '(dired-jump :which-key "dired")              ; Emacs: C-x d
+	"f" '(find-file :which-key "file")                ; Emacs: C-x C-f
+	"g" '(magit :which-key "git")                     ; Emacs: C-x g
+	"h" `(,help-map :which-key "help")                ; Emacs: C-h
+	"i" '(imenu :which-key "imenu")                   ; Emacs: M-g i
+	"j" '(bookmark-jump :which-key "jump")
+	"k" '(kill-this-buffer :which-key "kill")         ; Emacs: C-x k
+	"n" `(,narrow-map :which-key "narrow")            ; Emacs: C-x n
+	"o" '(ace-window :which-key "other")              ; Emacs: C-x o
+	"p" `(,project-prefix-map :which-key "project")   ; Emacs: C-x p
+	"q" '(evil-window-delete :which-key "quit")       ; Vim: :q
+	"r" '(recentf :which-key "recent")                ; Emacs: C-c r
+	"s" '(save-buffer :which-key "save")              ; Emacs: C-x s
+	"t" '(tab-bar-new-tab :which-key "tab")           ; Emacs: C-x t n
+	"w" `(,evil-window-map :which-key "window")       ; Vim: C-w
+	"y" '(clone-indirect-buffer-other-window :which-key "indirect"))
 
   ;; Map "C-c C-x" to ", x" for all letters "x". These are
   ;; generally keybindings defined by the current major mode,
   ;; and make a sensible set of default localleader bindings.
   (lmap
-    "a" (general-key "C-c C-a")
-    "b" (general-key "C-c C-b")
-    "c" (general-key "C-c C-c")
-    "d" (general-key "C-c C-d")
-    "e" (general-key "C-c C-e")
-    "f" (general-key "C-c C-f")
-    "g" (general-key "C-c C-g")
-    "h" (general-key "C-c C-h")
-    "i" (general-key "C-c C-i")
-    "j" (general-key "C-c C-j")
-    "k" (general-key "C-c C-k")
-    "l" (general-key "C-c C-l")
-    "m" (general-key "C-c C-m")
-    "n" (general-key "C-c C-n")
-    "o" (general-key "C-c C-o")
-    "p" (general-key "C-c C-p")
-    "q" (general-key "C-c C-q")
-    "r" (general-key "C-c C-r")
-    "s" (general-key "C-c C-s")
-    "t" (general-key "C-c C-t")
-    "u" (general-key "C-c C-u")
-    "v" (general-key "C-c C-v")
-    "w" (general-key "C-c C-w")
-    "x" (general-key "C-c C-x")
-    "y" (general-key "C-c C-y")
-    "z" (general-key "C-c C-z"))
+	"a" (general-key "C-c C-a")
+	"b" (general-key "C-c C-b")
+	"c" (general-key "C-c C-c")
+	"d" (general-key "C-c C-d")
+	"e" (general-key "C-c C-e")
+	"f" (general-key "C-c C-f")
+	"g" (general-key "C-c C-g")
+	"h" (general-key "C-c C-h")
+	"i" (general-key "C-c C-i")
+	"j" (general-key "C-c C-j")
+	"k" (general-key "C-c C-k")
+	"l" (general-key "C-c C-l")
+	"m" (general-key "C-c C-m")
+	"n" (general-key "C-c C-n")
+	"o" (general-key "C-c C-o")
+	"p" (general-key "C-c C-p")
+	"q" (general-key "C-c C-q")
+	"r" (general-key "C-c C-r")
+	"s" (general-key "C-c C-s")
+	"t" (general-key "C-c C-t")
+	"u" (general-key "C-c C-u")
+	"v" (general-key "C-c C-v")
+	"w" (general-key "C-c C-w")
+	"x" (general-key "C-c C-x")
+	"y" (general-key "C-c C-y")
+	"z" (general-key "C-c C-z"))
 
   ;; Map "C-c ?" to ", ?" for all symbols "?". This includes some
   ;; major-mode keybindings and most minor-mode keybindings. One
   ;; exception: ", ," is mapped to "C-c C-c" for simplicity.
   (lmap
-    "!"  (general-key "C-c !" )
-    "\"" (general-key "C-c \"")
-    "#"  (general-key "C-c #" )
-    "$"  (general-key "C-c $" )
-    "%"  (general-key "C-c %" )
-    "&"  (general-key "C-c &" )
-    "'"  (general-key "C-c '" )
-    "("  (general-key "C-c (" )
-    ")"  (general-key "C-c )" )
-    "*"  (general-key "C-c *" )
-    "+"  (general-key "C-c +" )
-    ","  (general-key "C-c C-c" )
-    "-"  (general-key "C-c -" )
-    "."  (general-key "C-c ." )
-    "/"  (general-key "C-c /" )
-    ":"  (general-key "C-c :" )
-    ";"  (general-key "C-c ;" )
-    "<"  (general-key "C-c <" )
-    "="  (general-key "C-c =" )
-    ">"  (general-key "C-c >" )
-    "?"  (general-key "C-c ?" )
-    "@"  (general-key "C-c @" )
-    "["  (general-key "C-c [" )
-    "\\" (general-key "C-c \\")
-    "]"  (general-key "C-c ]" )
-    "^"  (general-key "C-c ^" )
-    "_"  (general-key "C-c _" )
-    "`"  (general-key "C-c `" )
-    "{"  (general-key "C-c {" )
-    "|"  (general-key "C-c |" )
-    "}"  (general-key "C-c }" )
-    "~"  (general-key "C-c ~" )))
+	"!"  (general-key "C-c !" )
+	"\"" (general-key "C-c \"")
+	"#"  (general-key "C-c #" )
+	"$"  (general-key "C-c $" )
+	"%"  (general-key "C-c %" )
+	"&"  (general-key "C-c &" )
+	"'"  (general-key "C-c '" )
+	"("  (general-key "C-c (" )
+	")"  (general-key "C-c )" )
+	"*"  (general-key "C-c *" )
+	"+"  (general-key "C-c +" )
+	","  (general-key "C-c C-c" )
+	"-"  (general-key "C-c -" )
+	"."  (general-key "C-c ." )
+	"/"  (general-key "C-c /" )
+	":"  (general-key "C-c :" )
+	";"  (general-key "C-c ;" )
+	"<"  (general-key "C-c <" )
+	"="  (general-key "C-c =" )
+	">"  (general-key "C-c >" )
+	"?"  (general-key "C-c ?" )
+	"@"  (general-key "C-c @" )
+	"["  (general-key "C-c [" )
+	"\\" (general-key "C-c \\")
+	"]"  (general-key "C-c ]" )
+	"^"  (general-key "C-c ^" )
+	"_"  (general-key "C-c _" )
+	"`"  (general-key "C-c `" )
+	"{"  (general-key "C-c {" )
+	"|"  (general-key "C-c |" )
+	"}"  (general-key "C-c }" )
+	"~"  (general-key "C-c ~" )))
 
 (use-package kkp
   :custom
@@ -313,22 +311,29 @@
   :bind
   ("<f12>" . modus-themes-toggle))
 
-(use-package ivy
-  :custom
-  (enable-recursive-minibuffers t)
+(use-package vertico
   :config
-  (ivy-mode 1))
+  (vertico-mode 1)
+  (vertico-mouse-mode 1))
+
+(use-package vertico-directory
+  :after vertico
+  :ensure nil
+  :bind (:map vertico-map
+			  ("RET" . vertico-directory-enter)
+			  ("DEL" . vertico-directory-delete-char)
+			  ("M-DEL" . vertico-directory-delete-word))
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
+
+(use-package orderless
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
 
 (use-package swiper
   :bind
   ("C-s" . swiper))
-
-(use-package counsel
-  :config
-  (counsel-mode 1)
-  :bind
-  (:map minibuffer-local-map
-        ("C-r" . counsel-minibuffer-history)))
 
 (use-package ace-window)
 
@@ -340,11 +345,13 @@
   (initial-scratch-message "")
   (org-todo-keywords
    '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
-     (sequence "WAIT(w)" "HOLD(h)" "IDEA(*)" "|" "NOTE(-)" "STOP(s)")))
+	 (sequence "WAIT(w)" "HOLD(h)" "IDEA(*)" "|" "NOTE(-)" "STOP(s)")))
+  (org-adapt-indentation t)
   (org-agenda-files (list org-directory))
   (org-agenda-skip-deadline-if-done t)
   (org-agenda-skip-scheduled-if-done t)
   (org-agenda-span 'day)
+  (org-hide-leading-stars t)
   (org-agenda-start-on-weekday nil)
   (org-agenda-window-setup 'other-tab)
   (org-archive-location "::* Archive")
@@ -354,12 +361,11 @@
   (org-highlight-latex-and-related '(native latex script entities))
   (org-image-actual-width '(400))
   (org-startup-folded 'fold)
-  (org-adapt-indentation t)
   (org-tags-column -65)
   :config
   (defun +url-handler-zotero (link)
-    "Open a zotero:// link in the Zotero desktop app."
-    (start-process "zotero_open" nil "open" (concat "zotero:" link)))
+	"Open a zotero:// link in the Zotero desktop app."
+	(start-process "zotero_open" nil "open" (concat "zotero:" link)))
   (org-link-set-parameters "zotero" :follow #'+url-handler-zotero))
 
 (use-package org-download
@@ -371,16 +377,16 @@
   (org-download-timestamp "%Y%m%d%H%M%S")
   :config
   (defun +org-download-file-format (filename)
-    "Purely date-based naming of attachments."
-    (concat
-      (format-time-string org-download-timestamp)
-      "."
-      (file-name-extension filename)))
+	"Purely date-based naming of attachments."
+	(concat
+	 (format-time-string org-download-timestamp)
+	 "."
+	 (file-name-extension filename)))
   (setq org-download-file-format-function #'+org-download-file-format)
   (setq org-download-annotate-function (lambda (_link) ""))
   (org-download-enable)
   :bind (:map org-mode-map
-              ("M-v" . org-download-clipboard)))
+			  ("M-S-v" . org-download-clipboard)))
 
 (use-package org-babel
   :after org
@@ -444,25 +450,45 @@
   ((TeX-mode . turn-on-cdlatex)
    (org-mode . turn-on-org-cdlatex)))
 
+(use-package eglot
+  :custom
+  (eldoc-echo-area-prefer-doc-buffer t)
+  (eldoc-echo-area-use-multiline-p nil)
+  :config
+  (defun jabirali/eglot-ensure-in-project ()
+	"Run Eglot only if we're in a project."
+	(if (project-current) (eglot-ensure))))
+
 (use-package python
-  :after (outline evil general)
+  :after (outline evil general eglot)
   :config
   (defun +outline-python ()
-    "Fold Python code like Org-mode headings."
-    ;; Only fold definitions and decorators (not e.g. loops and conditions).
-    (setq outline-regexp
-          (rx (or
-               (group (group (* space)) bow (or "class" "def") eow)
-               (group (group (* space)) "@"))))
-    ;; Org-mode-like keybindings for cycling through outline states.
-    (evil-define-key 'motion 'local (kbd "<tab>")
-      (general-predicate-dispatch nil (derived-mode-p  'prog-mode) 'outline-cycle))
-    (evil-define-key 'motion 'local (kbd "<backtab>")
-      (general-predicate-dispatch nil (derived-mode-p 'prog-mode) 'outline-cycle-buffer))
-    ;; Enable the mode.
-    (outline-minor-mode 1))
+	"Fold Python code like Org-mode headings."
+	;; Only fold definitions and decorators (not loops and conditions).
+	(setq outline-regexp
+		  (rx (or
+			   (group (group (* space)) bow (or "class" "def") eow)
+			   (group (group (* space)) "@"))))
+	;; Org-mode-like keybindings for cycling through outline states.
+	(evil-define-key 'motion 'local (kbd "<tab>")
+	  (general-predicate-dispatch nil (derived-mode-p  'prog-mode) 'outline-cycle))
+	(evil-define-key 'motion 'local (kbd "<backtab>")
+	  (general-predicate-dispatch nil (derived-mode-p 'prog-mode) 'outline-cycle-buffer))
+	;; Enable the mode.
+	(outline-minor-mode 1))
   :hook
-  (python-mode . +outline-python))
+  (python-mode . jabirali/eglot-ensure-in-project))
+;; (python-mode . +outline-python))
+
+(use-package julia-mode)
+
+(use-package company
+  :after eglot
+  :hook (eglot-managed-mode . company-mode))
+
+(use-package yasnippet
+  :config
+  (yas-global-mode 1))
 
 (use-package dired
   :ensure nil
@@ -477,11 +503,20 @@
   :config
   (diredfl-global-mode 1))
 
+(use-package openwith
+  :config
+  (setq openwith-associations
+		'(("\\.\\(png\\|jpg\\|svg\\)$" "qlmanage -p" (file))
+		  ("\\.\\(pdf\\|docx\\|xlsx\\|pptx\\)$" "open" (file))))
+  (openwith-mode 1))
+
 (use-package gnuplot)
 
 (use-package hl-todo
   :hook
   (prog-mode . hl-todo-mode))
+
+(use-package gptel)
 
 (use-package magit
   :bind
@@ -489,11 +524,9 @@
   :custom
   (magit-diff-refine-hunk 'all)
   :config
-  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
+  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
+  ;; Integrate with Project.el.
+  (add-to-list 'project-switch-commands '(magit-project-status "Magit") t)
+  (keymap-set project-prefix-map "m" #'magit-project-status))
 
-(use-package openwith
-  :config
-  (setq openwith-associations
-         '(("\\.\\(png\\|jpg\\|svg\\)$" "qlmanage -p" (file))
-           ("\\.\\(pdf\\|docx\\|xlsx\\|pptx\\)$" "open" (file))))
-  (openwith-mode 1))
+(setq gc-cons-threshold (* 1024 1024))
