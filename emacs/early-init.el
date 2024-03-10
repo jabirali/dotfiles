fae293d8649bdc59dd3c3b0217014df24ae814cd
Jabir Ali Ouassou <jabir.ali.ouassou@hvl.no>
2024-03-10 21:02:02 +0100

    BREAKING CHANGE: went back to literate config

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,12 +1,2 @@
-;;; ~/.config/emacs/early-init.el
-
-;; Don't litter my init.el with the customization cache.
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (ignore-errors (load custom-file))
-
-;; Customize how new frames and windows look by default.
-(setq-default left-margin-width 2 right-margin-width 2)
-
-;; Improve performance.
-(setq gc-cons-threshold (* 128 1024 1024)
-	  read-process-output-max (* 1024 1024))
